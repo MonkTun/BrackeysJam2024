4f4974d900c41675607085412ae306fdc2d61a05
LucasWang <100511265+Novigradian@users.noreply.github.com>
2024-02-12 00:00:19 +0800

    basic movement and camera follow player

---
+++ BrackeysJam2024/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float initVelocity;
+    [SerializeField] private float maxVelocity;
+    [SerializeField] private float acceleration;
+    private float xVelocity;
+    private float yVelocity;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        xVelocity = 0f;
+        yVelocity = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Horizontal input
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                xVelocity = initVelocity * -1f;
+            }
+            xVelocity -=Mathf.Log(Mathf.Abs(xVelocity))*acceleration*Time.deltaTime;
+            if (xVelocity <= maxVelocity * -1f) 
+            {
+                xVelocity = maxVelocity*-1f;
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                xVelocity = initVelocity * 1f;
+            }
+            xVelocity += Mathf.Log(Mathf.Abs(xVelocity)) * acceleration * Time.deltaTime;
+            if (xVelocity >= maxVelocity * 1f)
+            {
+                xVelocity = maxVelocity;
+            }
+        }
+        else
+        {
+            xVelocity = 0f;
+        }
+
+        //Vertical Input
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                yVelocity = initVelocity * 1f;
+            }
+            yVelocity += Mathf.Log(Mathf.Abs(yVelocity)) * acceleration * Time.deltaTime;
+            if (yVelocity >= maxVelocity * 1f)
+            {
+                yVelocity = maxVelocity;
+            }
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                yVelocity = initVelocity * -1f;
+            }
+            yVelocity -= Mathf.Log(Mathf.Abs(yVelocity)) * acceleration * Time.deltaTime;
+            if (yVelocity <= maxVelocity * -1f)
+            {
+                yVelocity = maxVelocity*-1f;
+            }
+        }
+        else
+        {
+            yVelocity = 0f;
+        }
+
+        //Debug.Log(rb.velocity);
+        rb.velocity = new Vector2(xVelocity, yVelocity);
+    }
+}
