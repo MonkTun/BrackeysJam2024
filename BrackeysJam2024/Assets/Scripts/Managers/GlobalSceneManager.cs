23560b29208fccc97f9ee837c4a116ddc36d18ca
Tundraswan <75334356+MonkTun@users.noreply.github.com>
2024-02-11 09:31:18 -1000

    Merged SceneLoader into GlobalSceneManager
    Also add Global-Manager series into SystemManagger Prefab
    cuz i assumed that's what you were planning to do later

--- BrackeysJam2024/Assets/Scripts/Managers/GlobalSceneManager.cs
+++ BrackeysJam2024/Assets/Scripts/Managers/GlobalSceneManager.cs
@@ -5,21 +5,54 @@ using UnityEngine.SceneManagement;
 
 public class GlobalSceneManager : MonoBehaviour
 {
-    public static GlobalSceneManager instance;
-    private void Awake()
+    public static GlobalSceneManager Instance {  get; private set; }
+
+	[SerializeField] private GameObject _loadPanel;
+
+	private Coroutine _loadCoroutine;
+
+	private void Awake()
     {
-        if (instance != null)
+        if (Instance != null)
         {
             Destroy(gameObject);
         }
         else
         {
-            instance = this;
+            Instance = this;
             DontDestroyOnLoad(gameObject);
         }
     }
+
     public void OpenGame()
     {
         SceneManager.LoadScene("GameScene");
     }
+
+	// Hello Henry below is from my SceneLoader
+
+	public void OpenGameAsync(string sceneName) 
+	{
+		if (_loadCoroutine == null) return;
+
+		_loadCoroutine = StartCoroutine(LoadAsyncScene(sceneName));
+	}
+
+	private IEnumerator LoadAsyncScene(string sceneName)
+	{
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+		_loadPanel.SetActive(true);
+
+		//TODO Animation	
+
+		while (!asyncLoad.isDone)
+		{
+			yield return null;
+		}
+
+		_loadPanel.SetActive(false);
+
+		_loadCoroutine = null;
+	}
 }
