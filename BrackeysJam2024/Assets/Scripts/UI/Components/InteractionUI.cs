18ba9afe2a5c681a87749286fa8c507da8befbcf
Tundraswan <75334356+MonkTun@users.noreply.github.com>
2024-02-11 21:52:09 -1000

    InteractionUI

--- BrackeysJam2024/Assets/Scripts/UI/Components/InteractionUI.cs
+++ BrackeysJam2024/Assets/Scripts/UI/Components/InteractionUI.cs
@@ -9,18 +9,31 @@ public class InteractionUI : MonoBehaviour
     [SerializeField] private TMP_Text _interactionText;
 	[SerializeField] private CanvasGroup _interactionCanvasGroup;
 
+	[SerializeField] private float _snapSpeed = 10;
+	[SerializeField] private float _fadeSpeed = 2;
+
+	private Vector2 _lastInteractionPosition;
+
     public void UpdateIteraction(Vector2 interactionPosition, string itemName)
     {
+		if (_lastInteractionPosition == interactionPosition)
+		{
+			_interactionUI.transform.position = Vector2.Lerp(_interactionUI.transform.position, Camera.main.WorldToScreenPoint(interactionPosition), _snapSpeed * Time.deltaTime);
+		} 
+		else
+		{
+			_interactionUI.transform.position = Camera.main.WorldToScreenPoint(interactionPosition);
+		}
+
+		_lastInteractionPosition = interactionPosition;
+
 		_interactionText.text = "F - " + itemName;
 
 		_interactionCanvasGroup.alpha = 1;
-
-
-		_interactionUI.transform.position = Camera.main.WorldToScreenPoint(interactionPosition);
 	}
 
 	private void Update()
 	{
-		//_interactionCanvasGroup.alpha _
+		_interactionCanvasGroup.alpha -= Time.deltaTime * _fadeSpeed;
 	}
 }
