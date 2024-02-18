41cd7f464efdd81ce86a9c8891f4227860486455
Tundraswan <75334356+MonkTun@users.noreply.github.com>
2024-02-18 00:44:19 -1000

    item sizes fix + barricade fix

--- BrackeysJam2024/Assets/DoorHingeHelper.cs
+++ BrackeysJam2024/Assets/DoorHingeHelper.cs
@@ -12,28 +12,40 @@ public class DoorHingeHelper : MonoBehaviour
     private bool isLocked=true;
     private HingeJoint2D joint;
 
-    private float jointinitla;
+    private float _jointInitialRot;
+
+    private bool _barricaded;
+
+    //[SerializeField] private Transform _actualDoor;
 
     private void Awake()
     {
-        _initialPos = transform.position;
+        _initialPos = transform.localPosition;
         rb2D = GetComponent<Rigidbody2D>();
         mmf = GetComponent<MMF_Player>();
 		joint = GetComponent<HingeJoint2D>();
-        jointinitla = joint.jointAngle;
+        _jointInitialRot = joint.jointAngle;
 	}
     private void FixedUpdate() 
     {
         if (isLocked & rb2D.velocity.magnitude > 0.02f) { isLocked = false; mmf.PlayFeedbacks(); }
         if (!isLocked&&transform.localPosition.y * lastYPosition < 0) { rb2D.velocity = Vector2.zero; rb2D.angularVelocity = 0; isLocked = true; mmf.PlayFeedbacks(); }
-        rb2D.mass = isLocked ? 200 : 50;
+        if (_barricaded == false) rb2D.mass = isLocked ? 200 : 50;
         lastYPosition = transform.localPosition.y;
     }
 
     public void LockDoor()
     {
-        //isLocked = true;
-        //transform.localPosition = _initialPos;
+		transform.localPosition = _initialPos;
+		transform.localRotation = Quaternion.Euler(0, 0, _jointInitialRot);
+		rb2D.velocity = Vector2.zero;
+		rb2D.angularVelocity = 0;
+        _barricaded = true;
+        rb2D.mass = 1000;
+		//joint.enabled = false;
+
+		//isLocked = true;
+		//transform.localPosition = _initialPos;
 		//joint. = jointinitla; 
 		//rb2D.velocity = Vector2.zero;
 	}
